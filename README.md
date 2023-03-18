b029bd90f8f4710659a7328fcd90a509a204651c
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-03-18 19:59:58 +0200

    feat: board is dunamicly created instead of hardcode

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Tests about react projects on js and how to work with them
 
 - [x] For the current move only, show “You are at move #…” instead of a button  
 
-- [ ] Rewrite Board to use two loops to make the squares instead of hardcoding them.  
+- [x] Rewrite Board to use two loops to make the squares instead of hardcoding them.  
 
 - [ ] Add a toggle button that lets you sort the moves in either ascending or descending order.  
 
--- src/Board.jsx
+++ src/Board.jsx
@@ -2,65 +2,72 @@
 import Square from './Square'
 
 export default function Board({ xIsNext, squares, onPlay }) {
-    function handleClick(i) {
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      const nextSquares = squares.slice();
-      if (xIsNext) {
-        nextSquares[i] = "X";
-      } else {
-        nextSquares[i] = "O";
-      }
-      onPlay(nextSquares);
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
-  
-    const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
     } else {
-      status = "Next player: " + (xIsNext ? "X" : "O");
+      nextSquares[i] = "O";
     }
-  
-    return (
-      <>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </>
-    );
+    onPlay(nextSquares);
   }
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
+
+  const playTable = getTable(3,squares );
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      {playTable}
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+function getTable(boardRows, previousTable ){
+  const squersCount = boardRows * boardRows;
+
+  let squersArray = [];
+  for (let i = 0; i < squersCount; i++) {
+    squersArray.push((<Square value={previousTable[i]} onSquareClick={() => handleClick(i)}  key={i}/>))
+  }
+
+  let table = [];
+  let cutIndex=0;
+  for (let i = 0; i < boardRows; i++) {
+    table.push(<div key = {i} className="board-row">{squersArray.slice(cutIndex, cutIndex + boardRows)}</div>)
+    cutIndex+=boardRows;
+  }
+
+  return table;
+}
